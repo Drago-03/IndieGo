1063e97b19b0928e36aed8e06126f01cf485e8c7
Drago <hitmanagent404@gmail.com>
2024-11-19 19:10:34 +0530

    new commands and roles

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 
@@ -18,6 +19,22 @@ class Moderation(commands.Cog):
         await ctx.send(f'{member.name} has been banned. Reason: {reason}')
 
     @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'{user.name}#{user.discriminator} has been unbanned.')
+                return
+
+        await ctx.send(f'{member} was not found.')
+
+    @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount: int):
         await ctx.channel.purge(limit=amount + 1)
@@ -34,6 +51,33 @@ class Moderation(commands.Cog):
         
         await member.add_roles(muted_role)
         await ctx.send(f'{member.name} has been muted. Reason: {reason}')
+
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def unmute(self, ctx, member: discord.Member):
+        muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+        await member.remove_roles(muted_role)
+        await ctx.send(f'{member.name} has been unmuted.')
+
+    @commands.command()
+    @commands.has_permissions(manage_roles=True)
+    async def timeout(self, ctx, member: discord.Member, duration: int, *, reason=None):
+        muted_role = discord.utils.get(ctx.guild.roles, name="Muted")
+        if not muted_role:
+            muted_role = await ctx.guild.create_role(name="Muted")
+            for channel in ctx.guild.channels:
+                await channel.set_permissions(muted_role, speak=False, send_messages=False)
+        
+        await member.add_roles(muted_role)
+        await ctx.send(f'{member.name} has been muted for {duration} seconds. Reason: {reason}')
+        await asyncio.sleep(duration)
+        await member.remove_roles(muted_role)
+        await ctx.send(f'{member.name} has been unmuted after {duration} seconds.')
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def warn(self, ctx, member: discord.Member, *, reason=None):
+        await ctx.send(f'{member.name} has been warned. Reason: {reason}')
 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
