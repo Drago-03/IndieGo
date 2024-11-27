a7c22fd1229641a4579edbe2349c90ef3c11f7e7
Drago <hitmanagent404@gmail.com>
2024-11-28 04:09:38 +0530

    adagf

--- cogs/main.py
+++ cogs/main.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 import discord
 import os
 
+from config import INSTALL_URL
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -20,6 +22,12 @@ bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
+
+# Load cogs
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
 # Flask web server to keep the bot running
 app = Flask(__name__)
@@ -37,4 +45,5 @@ def keep_alive():
 
 if __name__ == "__main__":
     keep_alive()
+    bot.loop.create_task(load_cogs())
     bot.run(TOKEN)
